2fe07bd601f79b9dedde4133e129aec724fa7d7b
Joshua-Cherry <joshcherry31@gmail.com>
2023-02-13 16:51:16 -0600

    Need to find a way to change the window for the source

--- OBSheader.py
+++ OBSheader.py
@@ -7,3 +7,12 @@ password = "secret"
 START_RECORDING = "server_cmd_recording_start"
 STOP_RECORDING = "server_cmd_recording_stop"
 STOP_OBS = "server_cmd_obs_stop"
+DBG_FILE_PATH = "./"
+WBESOCKET_PATH = "wss://172.19.16.37:3101?client_type=screen_capture"
+SCENE_NAME = "Test Recording"
+SCOURCE_KIND = "window_capture"
+SOURCE_NAME = "recording"
+
+SOURCE = {"settings":{"window":"obs-websocket/protocol.md at 4.x-compat · obsproject/obs-websocket - Google Chrome:Chrome_WidgetWin_1:chrome.exe"}}
+
+# {"balance":0.5,"deinterlace_field_order":0,"deinterlace_mode":0,"enabled":true,"flags":0,"hotkeys":{},"id":"window_capture","mixers":0,"monitoring_type":0,"muted":false,"name":"recording","prev_ver":486539266,"private_settings":{},"push-to-mute":false,"push-to-mute-delay":0,"push-to-talk":false,"push-to-talk-delay":0,"settings":{"window":"obs-websocket/protocol.md at 4.x-compat · obsproject/obs-websocket - Google Chrome:Chrome_WidgetWin_1:chrome.exe"},"sync":0,"versioned_id":"window_capture","volume":1.0}
--- OBSserver.py
+++ OBSserver.py
@@ -4,9 +4,13 @@ import sys
 import time
 import asyncio
 import websocket
-from websocket import create_connection
+
+import win32gui, win32con
+
+import os
+import psutil
+
 import ssl
-import logging
 import OBSheader
 import json
 from obswebsocket import obsws, requests
@@ -14,71 +18,102 @@ from obswebsocket import obsws, requests
 CURRENT_STATE = "NULL"
 
 
-def on_message(ws, message):
-    print("Received message:", message)
-    handle_command(ws, message)
-
-def on_error(ws, error):
-    print("Received error:", error)
-
-def on_close(ws):
-    print("Connection closed")
-
-def on_open(ws):
-    print("Connection opened")
-
-
 async def main():
-
-    # ws = create_connection("wss://172.19.16.37:3101?client_type=screen_capture", sslopt={"cert_reqs": ssl.CERT_NONE})
-    # print("test")
-    ws = websocket.WebSocketApp("wss://172.19.16.37:3101?client_type=screen_capture", on_open = on_open, on_message = on_message, on_error = on_error, on_close = on_close)
-    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
-    # ws.send("Hello, World")
-    # print("Sent")
-    # print("Receiving...")
-    # while True:
-    #     try:
-    #         result =  ws.recv()
-    #         handle_command(result)
-    #     except Exception as inst:
-    #         print("Exception", inst)
-    #         ws.close()
-    #         exit()
-
+    # Starting and minimizing OBS
+    response = os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OBS Studio\OBS Studio (64bit).lnk")
+    time.sleep(2)
+    if response != None:
+        dbg("Response from starting OBS: " + response)
+        exit()
+    Minimize = win32gui.GetForegroundWindow()
+    win32gui.ShowWindow(Minimize, win32con.SW_MINIMIZE)
     
-    # try:
-    #     # stop = asyncio.Future()
-    #     async with websockets.connect("wss://172.19.16.37:3101?client_type=screen_capture", sslopt={"cert_reqs": ssl.CERT_NONE}) as wbsc:
-    #         # await stop # run forever
-    #         while true:
-    #             res = wbsc.rec(v)
-    #             print(res)
-
-    # except KeyboardInterrupt:
-    #     exit()
-
-def handle_command(ws, command):   
-    OBSWS = obsws(OBSheader.host, OBSheader.port, OBSheader.password)
     
-    OBSWS.connect()
-         
-    message = json.loads(command)      
-    print(message)    
-    if message['command'] == OBSheader.START_RECORDING:
-        OBSWS.call(requests.SetFilenameFormatting("TestName"))         
-        OBSWS.call(requests.StartRecording()) 
-        CURRENT_STATE = 'Recording'         
-    if message['command'] == OBSheader.STOP_RECORDING:
-        OBSWS.call(requests.StopRecording())
-        CURRENT_STATE = 'Not recording'
-    if message['command'] == OBSheader.STOP_OBS:
-        print("Attempting to shut down")
-        # ws.close()
-        OBSWS.disconnect()
-        # exit()
-
+    # Initializing obs and websocket connection here
+    obs_controller = OBS_controller()
+    time.sleep(2)
+    obs_controller.create_scene(OBSheader.SCENE_NAME)
+    obs_controller.create_source(OBSheader.SOURCE_NAME, OBSheader.SCOURCE_KIND, OBSheader.SCENE_NAME)
     
+    # I tried putting this line in the class but it did not work
+    # Look into this in the future
+    obs_controller.ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+    obs_controller.obsws.GetSourcesList()
+    
+    while True:
+        try:
+            obs_controller.dbg("Waiting for message.....")
+            result =  ws.recv()
+            obs_controller.handle_command(result)
+        except Exception as inst:
+            print("Exception", inst)
+            obs_controller.ws.close()
+            exit()
+
+class OBS_controller:
+    def __init__(self):
+        self.obsws = obsws(OBSheader.host, OBSheader.port, OBSheader.password)
+        self.obsws.connect()
+        self.scene_ids = []
+        
+        #########################################################################################################
+        # Connecting to Ismael's server and setting the ssl serts to none. Do we want to do this in production? #
+        #########################################################################################################
+        self.ws = websocket.WebSocketApp(OBSheader.WBESOCKET_PATH, on_open = self.on_open, on_message = self.on_message, on_error = self.on_error, on_close = self.on_close)
+
+    def handle_command(self, command):   
+        message = json.loads(command)      
+        print(message)    
+        if message['command'] == OBSheader.START_RECORDING:
+            self.obsws.call(requests.SetFilenameFormatting("TestName"))         
+            self.obsws.call(requests.StartRecording()) 
+            CURRENT_STATE = 'Recording'         
+        if message['command'] == OBSheader.STOP_RECORDING:
+            self.obsws.call(requests.StopRecording())
+            CURRENT_STATE = 'Not recording'
+        if message['command'] == OBSheader.STOP_OBS:
+            print("Attempting to shut down")
+            # self.ws.close()
+            self.obsws.disconnect()
+            for pid in (process.pid for process in psutil.process_iter() if process.name()=="obs64"):
+                os.kill(pid)
+            exit()
+            
+    def create_scene(self, name):
+        self.dbg("Creating scene " + name)
+        self.obsws.call(requests.CreateScene(name))
+    
+    def create_source(self, source_name, source_kind, scene_name):
+        self.dbg("Creating source " + source_name)
+        id = self.obsws.call(requests.CreateSource(source_name, source_kind, scene_name))
+        time.sleep(2)
+        self.obsws.call(requests.SetSourceSettings(source_name, source_kind, OBSheader.SOURCE))
+        print(self.obsws.call(requests.GetSourceSettings(source_name, source_kind)))
+        self.scene_ids.append(id)
+        
+    
+    def on_message(self, message):
+        self.dbg("Received message:", message)
+        self.handle_command(message)
+
+    def on_error(self, error):
+        self.dbg("Received error:", error)
+
+    def on_close(self, err):
+        self.dbg("Connection closed")
+
+    def on_open(self, err):
+        self.dbg("Connection opened")
+
+    def dbg(self, dbg_string):
+        print(dbg_string)
+        f = open(OBSheader.DBG_FILE_PATH + "OBSdbg.txt", "a")
+        f.write(dbg_string + "\n")
+        f.close()
+
+
+
+
 # async def handler(websocket, path):
 
 #     ws = obsws(OBSheader.host, OBSheader.port, OBSheader.password)
